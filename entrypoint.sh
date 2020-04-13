759a36dc7bb05477e4974af8832eba80368aa6f1
tvrcgo <tvrcgo@gmail.com>
2020-04-13 16:51:39 +0800

    feat: add run id in image tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-TAG=${GITHUB_REF##*/}-$(date +"%s")
+TAG=${GITHUB_REF##*/}-${GITHUB_RUN_ID}-$(date +"%s")
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin $DOCKER_REGISTRY
 docker build . --tag $DOCKER_REGISTRY/$DOCKER_REPO:$TAG
