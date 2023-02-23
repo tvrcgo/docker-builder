b21001d1c86b0eb4ccb9799224c65c8b756a6e1f
tvrcgo <tvrcgo@gmail.com>
2023-02-24 00:12:54 +0800

    fix: context

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ DFT_TAG=${GITHUB_REF##*/}-${GITHUB_RUN_ID}-$(date +"%s")
 TAG=${DOCKER_TAG:-$DFT_TAG}
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin $DOCKER_REGISTRY
-docker build ${DOCKER_BUILD_CONTEXT:-.} --file ${DOCKER_FILE:-Dockerfile} --tag $DOCKER_REGISTRY/$DOCKER_REPO:$TAG --tag $DOCKER_REGISTRY/$DOCKER_REPO:latest
+docker build --file ${DOCKER_FILE:-Dockerfile} --tag $DOCKER_REGISTRY/$DOCKER_REPO:$TAG --tag $DOCKER_REGISTRY/$DOCKER_REPO:latest ${DOCKER_BUILD_CONTEXT:-.}
 docker push $DOCKER_REGISTRY/$DOCKER_REPO:latest
 
 if [[ ! -z $DOCKER_TAG ]]; then
