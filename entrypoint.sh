ddeb1da560b0a3b2cd910eee080634a3ca8757e6
tvrcgo <tvrcgo@gmail.com>
2023-02-24 10:34:00 +0800

    fix: ctx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ DFT_TAG=${GITHUB_REF##*/}-${GITHUB_RUN_ID}-$(date +"%s")
 TAG=${DOCKER_TAG:-$DFT_TAG}
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin $DOCKER_REGISTRY
-docker build --file ${DOCKER_FILE:-Dockerfile} --tag $DOCKER_REGISTRY/$DOCKER_REPO:$TAG --tag $DOCKER_REGISTRY/$DOCKER_REPO:latest ${DOCKER_BUILD_CONTEXT:-.}
+docker build --file ${DOCKER_FILE:-Dockerfile} --tag $DOCKER_REGISTRY/$DOCKER_REPO:$TAG --tag $DOCKER_REGISTRY/$DOCKER_REPO:latest ${DOCKER_CTX}
+
 docker push $DOCKER_REGISTRY/$DOCKER_REPO:latest
 
 if [[ ! -z $DOCKER_TAG ]]; then
