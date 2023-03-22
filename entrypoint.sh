43a985d4fbaf9631657e916527ca6ed9d572e124
tvrcgo <tvrcgo@gmail.com>
2023-03-22 20:21:19 +0800

    fix: tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,17 @@ read -ra tags <<< "$INPUT_TAGS"
 # docker login
 echo "$INPUT_PASSWORD" | docker login -u "$INPUT_USERNAME" --password-stdin $INPUT_REGISTRY
 
-for tag in "${tags[@]}"; do
-  echo "<$tag>"
-  printf "%s" "-t $(echo $tag | xargs)"
-done
-
 # build and push
-tag_args=$(for tag in "${tags[@]}"; do printf "%s\t" "--tag $(echo $tag | xargs)"; done | xargs)
+tag_args=()
+for tag in "${tags[@]}"; do
+  tag_args+=(-t $tag)
+done
 
 cd $INPUT_CONTEXT
 docker buildx create --use
 docker buildx build \
   --file "$INPUT_DOCKERFILE" \
   --platform "$INPUT_PLATFORMS" \
-  $tag_args \
+  "${tag_args[@]}" \
   --push \
   .
