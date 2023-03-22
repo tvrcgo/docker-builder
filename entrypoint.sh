ef6ae25ad651bcbbe8c49808e92706b4e253810c
tvrcgo <tvrcgo@gmail.com>
2023-03-22 17:41:11 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,14 @@ echo "$INPUT_PASSWORD" | docker login -u "$INPUT_USERNAME" --password-stdin $INP
 
 # build and push
 tag_args=$(for tag in "${tags[@]}"; do printf "%s " "-t $tag"; done | sed 's/ *$//g')
+ls .
+echo $(pwd)
 
+cd $INPUT_CONTEXT
 docker buildx create --use
 docker buildx build \
   --file $INPUT_DOCKERFILE \
   --platform $INPUT_PLATFORMS \
   $tag_args \
   --push \
-  $INPUT_CONTEXT
+  .
