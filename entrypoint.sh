32b97cfcf77540868ffbe53e21721cd6c7f85efc
tvrcgo <tvrcgo@gmail.com>
2023-03-22 19:06:56 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "INPUT_TAGS=<$INPUT_TAGS>"
 IFS=','
 read -ra tags <<< "$INPUT_TAGS"
 
@@ -8,7 +9,7 @@ echo "$INPUT_PASSWORD" | docker login -u "$INPUT_USERNAME" --password-stdin $INP
 
 # build and push
 tag_args=$(for tag in "${tags[@]}"; do printf "%s " "-t $(echo $tag | xargs)"; done | xargs)
-echo "<$tag_args>"
+echo "tag_args=<$tag_args>"
 
 cd $INPUT_CONTEXT
 docker buildx create --use
